d064baf703dc0886c8ae7247deb16e2e82af6fbc
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-26 13:46:57 +0300

    v1.5

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,29 +1,31 @@
 import axios from "axios";
+import { result } from "lodash";
 
-axios.defaults.headers.common["x-api-key"] = "live_sW0PYRsVvFmIP6p9uFgJzQTR6aDBktZBD4fKfgnGbCKgtHdhwf7guufZawFBfnm7";
+const apiKey = 'live_sW0PYRsVvFmIP6p9uFgJzQTR6aDBktZBD4fKfgnGbCKgtHdhwf7guufZawFBfnm7'
+axios.defaults.headers.common["x-api-key"] = apiKey;
 
 
 export const fetchBreeds = () => {
-     return fetch('https://api.thecatapi.com/v1/breeds').then((response) => {
-          if (!response.ok) {
+     return axios.get('https://api.thecatapi.com/v1/breeds').then((response) => {
+          if (!response.data) {
                new Error(response.status);
           }
           else {
-               return response.json();
+               return response.data;
           }
      })
 }
 
 export const fetchCatByBreed = (id) => {
      let params = new URLSearchParams({
-          breed_ids: id
+          breed_ids: id,
      });
-     return fetch(`https://api.thecatapi.com/v1/images/search?${params.toString()}`).then((response) => {
-          if (!response.ok) {
+     return axios.get(`https://api.thecatapi.com/v1/images/search?${params}`).then((response) => {
+          if (!response.data) {
                new Error(response.status);
           }
           else {
-               return response.json();
+               return response.data[0];
           }
      })
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api'
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select'
+
+new SlimSelect({
+     select: '.breed-select'
+})
 
 const selector = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
 
 function renderBreeds(breeds) {
      const markup = breeds
@@ -11,6 +17,19 @@ function renderBreeds(breeds) {
           })
           .join("");
      selector.innerHTML = markup;
+}
+
+function renderInfo(info) {
+     let breed = info.breeds[0];
+     const markup = `<img class="image" src="${info.url}" alt="${breed.name}" height="${info.height}" width="${info.width}">
+     <div class="content">
+     <h2 class="title">${breed.name}</h2>
+     <p class="description">${breed.description}</p>
+     <p><span class="temperament">Temperament: </span>${breed.temperament}</p>
+     <p class="link">More details: <a href="${breed.wikipedia_url}" target="_blank">Wikipedia</a></p>
+     </div>`
+     catInfo.innerHTML = markup;
+     console.log(info)
 }
 
 fetchBreeds()
@@ -23,6 +42,11 @@ fetchBreeds()
 
 selector.addEventListener("change", () => {
      loader.classList.toggle('is-hidden');
-     fetchCatByBreed(selector.value)
+     fetchCatByBreed(selector.value).then((result) => {
+          loader.classList.toggle('is-hidden');
+          renderInfo(result);
+     })
+          .catch(() => error.classList.toggle('is-hidden'));
 });
+
 
